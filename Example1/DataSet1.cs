fe00b6089d9e777518f75ed177c23c4cf84c36f1
Beisenbek Baisakov <beisenbek@gmail.com>
2016-03-26 11:01:31 +0600

    init

---
+++ Example1/DataSet1.cs
@@ -0,0 +1,15 @@
+﻿namespace Example1
+{
+
+
+    partial class DataSet1
+    {
+    }
+}
+
+namespace Example1.DataSet1TableAdapters {
+    
+    
+    public partial class tbl_employeesTableAdapter {
+    }
+}
---
+++ Example1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet1TableAdapters.tbl_employeesTableAdapter t = new DataSet1TableAdapters.tbl_employeesTableAdapter();
+
+            foreach(var x in t.GetData())
+            {
+                Console.WriteLine(x.first_name);
+            }
+        }
+    }
+}
